ab4cf4f0f2fd6a68542f496c92097b513a7db8fb
Hojat Nasiri <hnasiri1997@gmail.com>
2024-12-15 21:31:56 +0330

    Create context

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+import UsersPage from "./pages/UsersPage";
+
+const users = [
+  { id: 1, firstName: "Hojat" },
+  { id: 2, firstName: "Hedieh" },
+  { id: 3, firstName: "Narges" },
+  { id: 4, firstName: "Hossein" },
+];
+
+const UserContext = createContext();
+
+function App() {
+  return (
+    <>
+      <h1>S16-Context</h1>
+
+      <UsersPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/UsersPage.jsx
@@ -0,0 +1,11 @@
+import UsersList from "../components/UsersList";
+
+function UsersPage() {
+  return (
+    <>
+      <UsersList />
+    </>
+  );
+}
+
+export default UsersPage;
